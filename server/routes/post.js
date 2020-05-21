292e2d2170417695cbf6f05dbcecb435569affdb
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-22 00:40:18 +0530

    Post completed

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const Post = mongoose.model("Post")
 const requireLogin = require('../middleware/requireLogin')
+
+router.get('/allpost', requireLogin, (req, res) => {
+    Post.find()
+        .populate('postedBy', "_id name")
+        .then(result => {
+            res.json({
+                result
+            })
+        })
+        .catch(error => {
+            console.log('error')
+        })
+})
 router.post('/createpost', requireLogin, (req, res) => {
 
     const { title, body } = req.body
@@ -11,13 +25,32 @@ router.post('/createpost', requireLogin, (req, res) => {
             error: "Please add all the fields"
         })
     }
-    console.log(req.user)
-    res.send('ok')
-        /*const post =new post({
-          title,
-          body,
-          postedBy
-        })*/
+    req.user.password = undefined
+    const post = new Post({
+        title,
+        body,
+        postedBy: req.user
+    })
+    post.save().then(result => {
+            res.json({ post: result })
+        })
+        .catch(err => {
+            console.log(error)
+        })
+
+})
+
+router.get('/mypost', requireLogin, (req, res) => {
+    Post.find({ postedBy: req.user._id })
+        .populate('postedBy', "_id name")
+        .then(data => {
+            res.json({
+                data
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
 
 })
 
