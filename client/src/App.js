cb230d2aef994308698532f816ae3583794f53db
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-22 02:22:14 +0530

    Components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import "./App.css"
+import Navbar from './components/Navbar'
+import {BrowserRouter,Route} from 'react-router-dom'
+import Home from "./components/layouts/Home";
+import Login from "./components/layouts/Login";
+import SignUp from "./components/layouts/SignUp";
+import Profile from "./components/layouts/Profile";
 function App() {
-    return <div > < /div>;
+    return (
+        <BrowserRouter>
+        <Navbar/>
+        <Route exact path='/'><Home/></Route>
+        <Route path='/login'><Login/></Route>
+        <Route path='/profile'><Profile/></Route>
+        <Route path='/register'><SignUp/></Route>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return(
+  <nav>
+    <div class="nav-wrapper">
+      <Link to="/" class="brand-logo">Socio</Link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Sign Up</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
+export default Navbar
