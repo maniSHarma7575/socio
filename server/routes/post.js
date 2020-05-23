e1c96042346858fa30a42f94189ed9cab0a98f36
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-23 21:00:38 +0530

    Like Unlike Backend added

--- server/routes/post.js
+++ server/routes/post.js
@@ -55,4 +55,33 @@ router.get('/mypost', requireLogin, (req, res) => {
 
 })
 
+router.put('/like',requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err)
+        {
+            return res.status(422).json({error:err})
+        }
+        res.json(result)
+    })
+})
+router.put('/unlike',requireLogin,(req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((err,result)=>{
+        if(err)
+        {
+            return res.status(422).json({error:err})
+        }
+        res.json(result)
+    })
+})
+
+
+
 module.exports = router
