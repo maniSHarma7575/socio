cbf45a45ed64e2b3d248bb393b75cffe35d482b9
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-24 12:26:45 +0530

    Api for comment added

--- server/models/posts.js
+++ server/models/posts.js
@@ -14,6 +14,10 @@ const postSchema = new mongoose.Schema({
         required:true
     },
     likes:[{type:ObjectId,ref:"User"}],
+    comments:[{
+        text:String,
+        postedBy:{type:ObjectId,ref:"User"}
+    }],
     postedBy: {
         type: ObjectId,
         ref: "User"
