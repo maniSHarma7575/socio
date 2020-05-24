d6f7fb0fe000975f0c7c5a5e0420d5cab90bc653
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-24 23:18:15 +0530

    Node api to see Multiple profile added

---
+++ server/routes/user.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const requireLogin = require('../middleware/requireLogin')
+const Post = mongoose.model("Post")
+const User=mongoose.model("User")
+
+router.get('/user/:id',requireLogin,(req,res)=>{
+  User.findOne({_id:req.param.id})
+  .select("-password")
+  .then(user=>{
+    Post.find({postedBy:req.param.id})
+    .populate("postedBy","_id name")
+    .exec((err,post)=>{
+      if(err)
+      {
+        return res.status(422).json({error:err})
+      }
+      res.json({user,post})
+    })
+  })
+  .catch(error=>{
+    return res.status(422).json({error:"User not found"})
+  })
+})
+module.exports = router
