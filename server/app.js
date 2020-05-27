93f000dab9d43d707f4442644d9ae6217918be66
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-27 20:25:26 +0530

    Sendgrid and  nodemailer added

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
+//const PORT = 5000
 const mongoose = require('mongoose')
 const { MONGO_URI } = require('./config/keys')
 require('./models/user')
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,8 +5,22 @@ const User = mongoose.model("User")
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../config/keys')
+const nodemailer=require('nodemailer')
+const sendgridTransport=require('nodemailer-sendgrid-transport')
 
+const transport=nodemailer.createTransport(sendgridTransport({
+    auth:{
+        api_key:"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"
+    }
+}))
 
+//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
+
+router.get('/',(req,res)=>{
+    res.json({
+        message:"Hello"
+    })
+})
 
 router.post('/signup', (req, res) => {
     const { name, email, password,photo } = req.body
@@ -32,6 +46,12 @@ router.post('/signup', (req, res) => {
                     })
                     user.save()
                         .then(user => {
+                            transport.sendMail({
+                                to:user.email,
+                                from:"sofowal733@ximtyl.com",
+                                subject:"Sign up successfull",
+                                html:"<h1>Welcome to instagram !Kudos for becoming the memeber of the Socio organisation</h1>"
+                            })
                             res.status(201).json({
                                 message: "User successfully added"
                             })
@@ -51,6 +71,7 @@ router.post('/signup', (req, res) => {
 })
 
 router.post('/signin', (req, res) => {
+    console.log('oka')
     const { email, password } = req.body
     if (!email || !password) {
         return res.status(422).json({
